955369c2c2a2a6a13523b1bbb4782dccd5916657
dhussein16 <dhusse16@gmail.com>
2023-04-16 17:43:12 -0500

    completed calculation of highest ranking and the comparison, added smaller functions to assist

--- src/main/java/PokerInfo.java
+++ src/main/java/PokerInfo.java
@@ -5,38 +5,23 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class PokerInfo implements Serializable {
-    //the players hand
-//    public Card cards[];
-//    private LinkedList<Card> deck;
-//    private Random shuffler;
-//    private boolean fold;
-//    private int player_id;
-//
-//    private int gamesPlay;
-//    private int numwin;
-//    private int playerAnte;
-//    private int playerPair;
-//    private int playerWage;
-
-    // the deck of cards
-    int[][] deck = new int[4][13];
-    int[][] playersHand = new int[2][3];
-    int[][] dealersHand = new int[2][3];
+    int[][] deck = new int[4][13];  // the deck of cards
+    int[][] playersHand = new int[2][3]; // first row of array tracks the suit of the card, second row is the actual card value
+    int[][] dealersHand = new int[2][3]; // first row of array tracks the suit of the card, second row is the actual card value
     int playersHighestCard = 0; // used to keep track of the highest order card in the hand of the player
     int dealersHighestCard = 0; // used to keep track of the highest order card for the dealer
+    int numberOfWins, totalEarnings, currentEarnings, ante, wager, pairPlus, playerRanking, dealerRanking;
+    boolean hasUserFolded, hasUserWon;
 
     // Constructor for the poker class
-    public PokerInfo(int p) {
-        cards = new Card[5];
-        this.dealersCards = new ArrayList<>();
-        this.playerAnte = 0;
-        this.player_id = 1;
-        this.playerPair = 0;
-        this.playerWage = 0;
-        this.numwin = 0;
-        this.gamesPlay = 0;
-        this.playersCards = new ArrayList<>();
-        this.fold = false;
+    public PokerInfo(int ante, int wager, int pairPlus) {
+        this.numberOfWins = 0;
+        this.totalEarnings = 0;
+        this.currentEarnings = 0;
+        this.hasUserFolded = false;
+        this.ante = ante;
+        this.wager = wager;
+        this.pairPlus = pairPlus;
     }
 
     // Function to calculate the highest rank for the cards that user and the dealer have
@@ -144,23 +129,26 @@ public class PokerInfo implements Serializable {
 
     // Function to compare the dealer's hand and the player's hand
     public void compareHands(int [][] playersHand, int [][] dealersHand, int playersHighestCard, int dealersHighestCard) {
-        int playersHandRanking = calculateHighestRank(playersHand, playersHighestCard);
-        int dealersHandRanking = calculateHighestRank(dealersHand, dealersHighestCard);
+        this.playerRanking = calculateHighestRank(playersHand, playersHighestCard);
+        this.dealerRanking = calculateHighestRank(dealersHand, dealersHighestCard);
 
-        if(playersHandRanking < dealersHandRanking){
+        if(this.playerRanking < this.dealerRanking){
             System.out.println("Sorry! You Lost!");
+            this.hasUserWon = false;
             // calculate money lost based on the player's bets
         }
-        else if(playersHandRanking > dealersHandRanking){
+        else if(this.playerRanking > this.dealerRanking){
             System.out.println("Congrats, you win!");
+            this.hasUserWon = true;
+            // calculate money won based on user's bets
         }
         else{
             System.out.println("Sorry, you tied. No money lost tho!");
+            this.hasUserWon = false;
         }
     }
 
-    // Funtion to load the deck for calculations
-    // Hearts = index 0, Spades = index 1, Clubs = index 2, Diamonds = index 3
+    // Funtion to load the deck for calculations, Hearts = index 0, Spades = index 1, Clubs = index 2, Diamonds = index 3
     // Aces are considered as card 1
     private void loadDeck(int[][] deck){
         for(int i = 0; i < 4; i++){
@@ -170,9 +158,38 @@ public class PokerInfo implements Serializable {
         }
     }
 
-    public int determineWinningsCurrent(){
-        //
-        return 0;
+    // Function to determine the winnings that the user gets if they win
+    public void determineWinningsCurrent(int wager, int ante, int pairPlus, boolean hasUserWon, boolean hasUserFolded){
+        if(hasUserFolded){
+            this.currentEarnings -= wager - ante - pairPlus;
+        }
+        else{
+            if(hasUserWon){
+                switch(this.playerRanking){
+                    case 5: // straight flush 40 - 1
+                        this.currentEarnings = 40 * (wager + ante + pairPlus);
+                        break;
+                    case 4: // three of a kind 30 - 1
+                        this.currentEarnings = 30 * (wager + ante + pairPlus);
+                        break;
+                    case 3: // straight 6 - 1
+                        this.currentEarnings = 6 * (wager + ante + pairPlus);
+                        break;
+                    case 2: // flush 3 - 1
+                        this.currentEarnings = 3 * (wager + ante + pairPlus);
+                        break;
+                    case 1: // pair 1 -1 (double)
+                        this.currentEarnings = 2 * (wager + ante + pairPlus);
+                        break;
+                    default: // high order (which I think they just get their money back);
+                        this.currentEarnings = (wager + ante + pairPlus);
+                        break;
+                }
+            }
+            else{ // the user played but lost
+                this.currentEarnings -= wager - ante - pairPlus;
+            }
+        }
     }
 
     private int updateUserCards(){
